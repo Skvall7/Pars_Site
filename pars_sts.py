fe0915333f8edcf3b77527ca13dcf205d6c0b512
Skvall <antonalizee@yandex.ru>
2022-07-01 15:22:02 +0300

    Рабочий вариант на 1 пользователя.

--- pars_sts.py
+++ pars_sts.py
@@ -3,21 +3,21 @@ import datetime
 import pymongo
 from bs4 import BeautifulSoup
 
+client = pymongo.MongoClient('localhost', 27017)
+base = client['sts_db']
+discharge = base['discharge']
+user_login_password = base['user_login_password']
+
 
 class Sts:
     url = 'http://10.78.78.251/'
     session = requests.session()
     result = []
-    client = pymongo.MongoClient('localhost', 27017)
-    base = client['sts_db']
-    discharge = base['discharge']
-    user_login_password = base['user_login_password']
 
-    def auth(self):
+    def auth(self, login, password):
         params = {'_csrf': '',
-                  'LoginForm[username]': 'aoka19@im.ru20',
-                  'LoginForm[password]': 'Aoka12346',
-                  'login-button': '',
+                  'LoginForm[username]': login,
+                  'LoginForm[password]': password,
                   'close-session': 1,
                   'close-not-verification': 1
                   }
@@ -28,6 +28,7 @@ class Sts:
         csrf = str(soup.find('input'))[-59:-3]
         params.update({'_csrf': csrf})
         session.post(url, params)
+
         return
 
     def take(self):
@@ -44,17 +45,23 @@ class Sts:
             self.result.append(info)
         self.result.pop(0)
         self.result.pop(0)
-        print(self.result)
-        return self.result
+        return
 
-    def record_db(self):
+    def record_db(self, dis):
         for info in self.result:
             base_dict = {'id_sts': info[3], 'date': info[5], 'link': info[4], 'props': info[:2]}
-            print(self.discharge.insert_one(base_dict).inserted_id)
+            print(dis.insert_one(base_dict).inserted_id, '- успешно занесено в базу')
+        return
 
 
 if __name__ == '__main__':
     sts = Sts()
-    sts.auth()
-    sts.take()
-    sts.record_db()
+    item = user_login_password.find({})
+    for r in range(1):
+        try:
+            user = [*item[r].values()]
+            sts.auth(user[1], user[2])
+            sts.take()
+            sts.record_db(discharge)
+        except:
+            break
